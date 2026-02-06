29d9819feeb4857a386339c75f41bff1dbb9c0f6
tda45 <tahadikbas45@gmail.com>
2026-02-06 16:35:01 +0300

    Kayıt Sistemi Yok

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("chat.db");
-
-// DB tablosunu oluştur
-db.run(`
-  CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user TEXT,
-    text TEXT,
-    time TEXT
-  )
-`);
-
 app.use(express.static("public"));
+
+let messages = []; // RAM tabanlı mesajlar
 
 io.on("connection", (socket) => {
 
@@ -24,31 +13,26 @@ io.on("connection", (socket) => {
     socket.username = username;
 
     // Mesaj geçmişini gönder
-    db.all("SELECT * FROM messages ORDER BY id ASC", [], (err, rows) => {
-      if (!err) socket.emit("chatHistory", rows);
-    });
+    socket.emit("chatHistory", messages);
 
-    // Kullanıcı katıldı mesajı
-    const joinMsg = {
+    // Katılma mesajı
+    io.emit("message", {
       user: "Chatter-LiveChat",
       text: `${username} katıldı`,
-      time: new Date().toLocaleTimeString("tr-TR", { hour: "2-digit", minute: "2-digit" })
-    };
-    io.emit("message", joinMsg);
+      time: new Date().toLocaleTimeString("tr-TR", { hour:"2-digit", minute:"2-digit" })
+    });
   });
 
   socket.on("chatMessage", (msg) => {
     if (!socket.username) return;
 
-    const time = new Date().toLocaleTimeString("tr-TR", { hour: "2-digit", minute: "2-digit" });
-    const data = { user: socket.username, text: msg, time };
+    const data = {
+      user: socket.username,
+      text: msg,
+      time: new Date().toLocaleTimeString("tr-TR", { hour:"2-digit", minute:"2-digit" })
+    };
 
-    // Mesajı DB'ye ekle
-    db.run("INSERT INTO messages (user, text, time) VALUES (?, ?, ?)", [data.user, data.text, data.time], (err) => {
-      if (err) console.error(err);
-    });
-
-    // Mesajı yay
+    messages.push(data); // RAM’de sakla
     io.emit("message", data);
   });
 });
